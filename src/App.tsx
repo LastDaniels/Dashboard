46dd2a4ff070e356cf63f816b888f56d54411bd4
LastDaniels <dannypicon.a0@gmail.com>
2024-06-19 10:31:16 -0500

    Components update

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,24 @@ import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import './App.css'
-
+import Indicator from './components/Indicator';
+import Summary from './components/Summary';
+import BasicTable from './components/BasicTable';
 function App() {
   
 
   return (
     <Grid container spacing={5}>
-	      <Grid xs={12} sm={4} md={3} lg={2}>1</Grid>
-	      <Grid xs={6} sm={4} md={3} lg={2}>2</Grid>
-	      <Grid xs={6} sm={4} md={3} lg={2}>3</Grid>
-	      <Grid xs={12} sm={4} md={3} lg={2}>4</Grid>
-	      <Grid xs={6} sm={4} md={6} lg={2}>5</Grid>
-	      <Grid xs={6} sm={4} md={6} lg={2}>6</Grid>
+	      <Grid xs={12} sm={4} md={3} lg={2}><Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /></Grid>
+		 
+	      <Grid xs={12} sm={4} md={3} lg={4}><Summary></Summary></Grid>
+		  
+	      <Grid xs={12} sm={4} md={3} lg={6}><BasicTable /></Grid>
+	     
+		  
+		  
+		  
+		  
 	    </Grid>
     )
 }
