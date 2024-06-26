3e9bea604cf68c403a565f2f978b866ce76631b0
Daniel <dannypicon.a04@gmail.com>
2024-06-25 20:46:40 -0500

    update de control y chart

--- src/App.tsx
+++ src/App.tsx
@@ -6,32 +6,40 @@ import './App.css'
 import Indicator from './components/Indicator';
 import Summary from './components/Summary';
 import BasicTable from './components/BasicTable';
-function App() {
-  
+import WeatherChart from './components/WeatherChart';
+import ControlPanel from './components/ControlPanel';
 
-  return (
-    <Grid container spacing={5}>
-	      <Grid xs={12} sm={4} md={3} lg={3}><Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /></Grid>
-		  
-	      <Grid xs={12} sm={4} md={3} lg={3}><Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /></Grid>
-		 
-		 
-	      <Grid xs={12} sm={4} md={3} lg={3}><Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /></Grid>
-		 
-		 
-	      <Grid xs={12} sm={4} md={3} lg={3}><Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /></Grid>
-		 
-		 
-	      <Grid xs={12} sm={4} md={3} lg={4}><Summary></Summary></Grid>
-		  
-	      <Grid xs={12} sm={4} md={3} lg={8}><BasicTable /></Grid>
-	     
-		  
-		  
-		  
-		  
-	    </Grid>
-    )
+function App() {
+
+
+	return (
+		<Grid container spacing={5}>
+			<Grid xs={12} sm={4} md={3} lg={3}><Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /></Grid>
+
+			<Grid xs={12} sm={4} md={3} lg={3}><Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /></Grid>
+
+
+			<Grid xs={12} sm={4} md={3} lg={3}><Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /></Grid>
+
+
+			<Grid xs={12} sm={4} md={3} lg={3}><Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} /></Grid>
+
+
+			<Grid xs={12} sm={4} md={3} lg={4}><Summary></Summary></Grid>
+
+			<Grid xs={12} sm={4} md={3} lg={8}><BasicTable /></Grid>
+			<Grid xs={12} lg={2}>
+				<ControlPanel />
+			</Grid>
+			<Grid xs={12} lg={10}>
+				<WeatherChart></WeatherChart>
+			</Grid>
+
+
+
+
+		</Grid>
+	)
 }
 
 export default App
